bda573e4f8f00de96739ac947e992071ddacc7de
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-04-15 20:52:13 -0300

    chore: create component modal

--- components/shared/AppModal.vue
+++ components/shared/AppModal.vue
@@ -1,10 +1,32 @@
 <template>
-  <div class="w-screen h-screen bg from-slate-200">
-    <div class="w-auto h-auto relative">
-      <BaseIcon class="absolute right-0 top-4" name="x" />
-      <slot />
+  <transition name="fade-up-down">
+    <div
+      v-if="show"
+      class="
+        w-screen
+        h-screen
+        bg-gradient-to-t
+        from-black
+        z-10
+        fixed
+        flex
+        justify-center
+        items-center
+        top-0
+        left-0
+      "
+    >
+      <div class="w-2/4 relative">
+        <div
+          @click="close"
+          class="absolute -right-3 -top-3 bg-green-500 rounded-full p-2"
+        >
+          <BaseIcon class="text-primary-light cursor-pointer" name="x" />
+        </div>
+        <slot />
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -15,5 +37,43 @@ export default {
   components: {
     BaseIcon,
   },
+
+  data() {
+    return {
+      show: false,
+    };
+  },
+
+  mounted() {
+    this.$nuxt.$on("dialogProjectImage", () => {
+      this.show = true;
+    });
+  },
+  beforeDestroy() {
+    this.$nuxt.$off("dialogProjectImage");
+  },
+
+  methods: {
+    close() {
+      this.show = false;
+    },
+  },
 };
 </script>
+
+<style scoped>
+.fade-up-down-enter-active {
+  transition: all 0.3s ease;
+}
+.fade-up-down-leave-active {
+  transition: all 0.3s ease;
+}
+.fade-up-down-enter {
+  transform: translateY(10%);
+  opacity: 0;
+}
+.fade-up-down-leave-to {
+  transform: translateY(10%);
+  opacity: 0;
+}
+</style>
