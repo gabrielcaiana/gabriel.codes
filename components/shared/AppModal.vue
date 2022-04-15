4eed8a5f5f03b82968dadf5c636dc66925229638
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-04-15 20:01:04 -0300

    chore: init modal component

---
+++ components/shared/AppModal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-screen h-screen bg from-slate-200">
+    <div class="w-auto h-auto relative">
+      <BaseIcon class="absolute right-0 top-4" name="x" />
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseIcon from "@/components/reusable/BaseIcon.vue";
+export default {
+  name: "AppModal",
+
+  components: {
+    BaseIcon,
+  },
+};
+</script>
