aa4bfa239b68e6f0b225c92b3a7018b8c46a06cc
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-04-15 18:25:37 -0300

    chore: create baseIcon

---
+++ components/reusable/BaseIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-on="$listeners">
+    <i :height="size" :width="size" :data-feather="name" :stroke="color"></i>
+  </div>
+</template>
+
+<script>
+import feather from "feather-icons";
+
+export default {
+  name: "BaseIcon",
+  props: {
+    name: {
+      type: String,
+      default: null,
+    },
+    color: {
+      type: String,
+      default: "white",
+    },
+    size: {
+      type: String,
+      default: "24",
+    },
+  },
+
+  mounted() {
+    feather.replace();
+  },
+  updated() {
+    feather.replace();
+  },
+};
+</script>
