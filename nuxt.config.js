df79e6ab825ec4b11bd1b711fe56308c84e037fe
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-06-04 20:14:17 -0300

    chore: update meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,25 +4,33 @@ export default {
     classSuffix: "",
   },
 
+  env: {
+    baseURL: process.env.BASE_URL
+  },
+
   head: {
-    title: "Gabriel Caiana - Website",
-    htmlAttrs: {
-      lang: "en",
-    },
+    titleTemplate: '%s - Gabriel Caiana',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: `
+        Bem-vindo(a) ao meu site Para quem não me conhece, meu nome é Gabriel Caiana, sou um desenvolvedor Frontend apaixonado por tecnologia e design.
+        Sou formado em Análise e desenvolvimento de sistemas pela FIAP, além de possuir diversas formações de especialização em tecnologias de Frontend, Design e UX.
+      ` },
+      { name: 'twitter:title', content: 'Gabriel Caiana - Website'},
+      { name: 'twitter:description', content: 'Sou desenvolvedor frontend apaixonado por tecnologia, design e inovação digital.'},
+      { name: 'twitter:image', content: './static/profile.png'},
+      { name: 'twitter:card', content: 'summary_large_image'},
+      { hid: 'og-type', property: 'og:type', content: 'website' },
+      { hid: 'og-title', property: 'og:title', content: 'Gabriel Caiana - Website' },
+      { hid: 'og-desc', property: 'og:description', content: 'Sou desenvolvedor frontend apaixonado por tecnologia, design e inovação digital.' },
+      { hid: 'og-image', property: 'og:image', content: './static/profile.png'},
+      { hid: 'og-url', property: 'og:url', content: 'https://gabrielcaiana.com' } 
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }]
   },
 
   css: ["~/assets/app.css"],
-
-  plugins: [
-    // "~/plugins/back-to-top.js"
-  ],
 
   components: true,
 
